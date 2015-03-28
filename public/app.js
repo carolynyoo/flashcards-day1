58a6f2c1e79d2f491fc6f50fde02d4ef66aff096
normchou <normchou@users.noreply.github.com>
2015-03-28 15:00:30 -0400

    wrong answer choices disapper

--- public/app.js
+++ public/app.js
@@ -1,50 +1,35 @@
-var app = angular.module('flashcards', {
-  // setup: function() {
-  //   // setup for flashcards 
-  // },
-  // teardown: function() {
-  //   //teardown for flashcards
-  // }
-});
+var app = angular.module('flashcards', []);
 
 app.controller('MainController', function($scope) {
   $scope.flashCards = [
     {
         question: 'What is Angular?',
         answers: [
-            { text: 'A front-end framework for great power!', correct: true },
-            { text: 'Something lame, who cares, whatever.', correct: false },
-            { text: 'Some kind of fish, right?', correct: false }
+            { text: 'A front-end framework for great power!', correct: true, renderAnswer: true},
+            { text: 'Something lame, who cares, whatever.', correct: false, renderAnswer: true  },
+            { text: 'Some kind of fish, right?', correct: false, renderAnswer: true  }
         ]
     },
     {
         question: 'What is a controller?',
         answers: [
-            { text: 'Something that manages my front-end routes', correct: false },
-            { text: 'A function that allows me to manage a scope', correct: true },
-            { text: 'An Angular template', correct: false }
+            { text: 'Something that manages my front-end routes', correct: false, renderAnswer: true  },
+            { text: 'A function that allows me to manage a scope', correct: true, renderAnswer: true  },
+            { text: 'An Angular template', correct: false, renderAnswer: true  }
         ]
     },
     {
         question: 'What does {{ }} do?',
         answers: [
-            { text: 'It runs some Javascript', correct: false },
-            { text: 'It looks for variables in HTML', correct: false },
-            { text: 'It runs an Angular expression that accesses my $scope', correct: true }
+            { text: 'It runs some Javascript', correct: false, renderAnswer: true  },
+            { text: 'It looks for variables in HTML', correct: false, renderAnswer: true  },
+            { text: 'It runs an Angular expression that accesses my $scope', correct: true, renderAnswer: true  }
         ]
       }
     ];
-    console.log('Main controller')
 })
+
 app.controller('FlashCardController', function ($scope) {
-  // $scope.flashCard = {
-  //   question: 'What is Angular?',
-  //   answers: [
-  //       { text: 'A front-end framework for great power!', correct: true, renderAnswer: true },
-  //       { text: 'Something lame, who cares, whatever.', correct: false, renderAnswer: true },
-  //       { text: 'Some kind of fish, right?', correct: false, renderAnswer: true }
-  //   ]
-  // };
   $scope.answered = false; 
   $scope.answeredCorrectly = false;
 
@@ -64,7 +49,6 @@ app.controller('FlashCardController', function ($scope) {
     } else {
       userAnswer.renderAnswer = false;
     }
-    // console.log($scope.flashCard.answers);
   };
 });
 
