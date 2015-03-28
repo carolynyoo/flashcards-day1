13921d3a2ddb8345a172916eb714e07343e34d46
Carolyn Yoo <yoo.carolyn@gmail.com>
2015-03-28 12:32:49 -0400

    On click angular method

--- public/app.js
+++ public/app.js
@@ -1,8 +1,22 @@
-angular.module('flashcards', {
-  setup: function() {
-    // setup for flashcards 
-  },
-  teardown: function() {
-    //teardown for flashcards
-  }
+var app = angular.module('flashcards', {
+  // setup: function() {
+  //   // setup for flashcards 
+  // },
+  // teardown: function() {
+  //   //teardown for flashcards
+  // }
+});
+
+app.controller('FlashCardController', function ($scope) {
+  $scope.flashCard = {
+    question: 'What is Angular?',
+    answers: [
+        { text: 'A front-end framework for great power!', correct: true },
+        { text: 'Something lame, who cares, whatever.', correct: false },
+        { text: 'Some kind of fish, right?', correct: false }
+    ]
+  };
+  $scope.answerQuestion = function (userAnswer) {
+    console.log(userAnswer.correct);
+  };
 });
